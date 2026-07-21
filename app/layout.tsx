e07b8535329b24bb023609fe4a870d6316f9a01c
DisisuPeft <defp240899@gmail.com>
2026-07-20 21:32:51 -0600

    add dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Provider from "@/redux/provider";
 import Setup from "./utils/auth/setup";
 import AlertSystem from "./utils/alert/alert";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -42,7 +43,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className={`${poppins.variable} ${sourceSans.variable}`}>
+      <head>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K6X6B699');`,
+          }}
+        />
+      </head>
       <body className="antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K6X6B699"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Provider>
           <Setup />
           <Analytics />
